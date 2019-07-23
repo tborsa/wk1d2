dc7661bc2defbbfd895000934d5b065bf6c8a2c0
travis <travis.borsa@gmail.com>
2019-07-23 11:18:57 -0700

    finished get arguments function

--- sum.js
+++ sum.js
@@ -1,6 +1,11 @@
 // grab all the command line arguments
 // discard the first two arguments
 
+const getArguments = function() {
+  return process.argv.slice(2);
+};
+console.log(getArguments());
+
 
 // Change the arguments into numbers
 // Check if the argument is a valid integer
