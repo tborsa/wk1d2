71ba9a8f7e394db07da669f395ec35b4a44a7e6f
travis <travis.borsa@gmail.com>
2019-07-23 11:45:48 -0700

    added numberize function to check array of arguments

--- sum.js
+++ sum.js
@@ -4,12 +4,26 @@
 const getArguments = function() {
   return process.argv.slice(2);
 };
-console.log(getArguments());
-
+//console.log(getArguments());
 
 // Change the arguments into numbers
 // Check if the argument is a valid integer
-  
+const numberize = function(arrArguments) {
+  let integerArray = [];
+  for (let argument of arrArguments) {
+    let maybeNumber = Number(argument);
+    if(maybeNumber && Number.isInteger(maybeNumber)){
+      integerArray.push(maybeNumber);
+    }else{
+      // it is not a number
+      console.log("error not an integer");
+    }
+  }
+  return integerArray;
+};
+
+const testArray = ["one", 2 , "dog"];
+console.log(numberize(testArray));
 
 // call a function to add the numbers
 // looping through arguments/numbers
