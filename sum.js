8ba1114732c618e76b2270328168b4294478e25b
travis <travis.borsa@gmail.com>
2019-07-23 10:57:11 -0700

    broke down the problem into functional steps

---
+++ sum.js
@@ -0,0 +1,11 @@
+// grab all the command line arguments
+// discard the first two arguments
+
+
+// Change the arguments into numbers
+// Check if the argument is a valid integer
+  
+
+// call a function to add the numbers
+// looping through arguments/numbers
+
